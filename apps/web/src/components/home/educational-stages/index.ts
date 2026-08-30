344527d9886e440a6fb7c338c7dcbf35712611b2
Mahmoud HabaZza <habazzaofficial@gmail.com>
2026-08-31 02:40:21 +0300

    feat(stages): commit before finishing task

--- apps/web/src/components/home/educational-stages/index.ts
+++ apps/web/src/components/home/educational-stages/index.ts
@@ -1,2 +1,4 @@
 export { EducationalStagesSection } from "./EducationalStagesSection";
+export { StageMilestone } from "./StageMilestone";
+export { StageMilestoneSkeleton } from "./StageMilestoneSkeleton";
 export type { EducationalStage } from "./types";
